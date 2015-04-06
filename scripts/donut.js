5c8dc6df5daa82bfc28cf4dcbfbc44b0be8145ca
Molly Kent <kent.molly@gmail.com>
2015-04-06 15:41:46 -0700

    fix the notes mess

--- scripts/donut.js
+++ scripts/donut.js
@@ -1,11 +1,4 @@
 (function() {
-
-
-
-	/*DonutShop.prototype.isOpen(time) {
-		return (time > this.opens && time < this.closes);
-	};*/
-
 	var DonutShop = function(location, minCust, maxCust, avgDonPurch, opens, closes) {
 		this.location = location;
 		this.minCust = minCust;
@@ -16,13 +9,19 @@
 		hourlyTotals = [];
 	}
 
-	var shops = [];
+	var downtown = new DonutShop ('Downtown', 8, 43, 4.50);
+	var capHill = new DonutShop ('Capitol Hill', 4, 37, 2.00);
+	var slu = new DonutShop ('South Lake Union', 9, 23, 6.33);
+	var wedge = new DonutShop ('Wedgewood', 2, 28, 1.25);
+	var ballard = new DonutShop ('Ballard', 8, 58, 3.75);
+
+	/*var shops = [];
 
 	shops.push(new DonutShop('Downtown', 8, 43, 4.50, 6, 18))
 	shops.push(new DonutShop('Capitol Hill', 4, 37, 2.00, 6, 18))
 	shops.push(new DonutShop('South Lake Union', 9, 23, 6.33, 6, 18))
 	shops.push(new DonutShop('Wedgewood', 2, 28, 1.25, 6, 18))
-	shops.push(new DonutShop('Ballard', 8, 58, 3.75, 6, 18))
+	shops.push(new DonutShop('Ballard', 8, 58, 3.75, 6, 18))*/
 
 	DonutShop.prototype.generateRandom = function () {
 		customers = Math.floor(Math.random() * (this.maxCust - this.minCust)) + this.minCust;
@@ -35,11 +34,11 @@
 		return donutsSold;
 	}
 
-	for (var hours = 7; hours < 18; hours++) {//this should loop thru the array of instances
-		//this.generateRandom();
+	for (var hours = 7; hours < 18; hours++) {
+		downtown.generateRandom();
 		console.log('Hour: ' + hours);
 		console.log('Customers: ' + customers);
-		//this.hourlyDonuts();
+		downtown.hourlyDonuts();
 		this.hourlyTotals.push(donutsSold);
 		console.log('Donuts sold that hour: ' + donutsSold);
 	};
