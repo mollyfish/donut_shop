0df46f3ac50a652876beb657d1d61ffc16046cb5
Scott Schmidt <sjschmidt44@gmail.com>
2015-04-08 13:29:34 -0700

    Add minor formatting fixes, changed indents to spaces, and removed unnecessary white space.

--- scripts/easterEggs.js
+++ scripts/easterEggs.js
@@ -1,21 +1,14 @@
 (function() {
   var DonutShop = window.DonutShop;
-
   var tableHeight;
-
   var dailyTotals = [];
   var el = document.getElementsByClassName('daily-total');
-  console.log(rows);
 
-    for (var i = 0; i < el.length; i++) {
-      dailyTotals.push(Number.parseInt(el[i].innerHTML));
-    } 
-    console.log(dailyTotals);
-    var topSeller = Math.max(dailyTotals[0], dailyTotals[1], dailyTotals[2], dailyTotals[3], dailyTotals[4], dailyTotals[5]);
-    console.log(topSeller);
-    var tableHeight = document.getElementsByTagName('tr');
-    var rows = tableHeight.length;
-  
+  for (var i = 0; i < el.length; i++) {
+    dailyTotals.push(Number.parseInt(el[i].innerHTML));
+  }
 
-  
+  var topSeller = Math.max(dailyTotals[0], dailyTotals[1], dailyTotals[2], dailyTotals[3], dailyTotals[4], dailyTotals[5]);
+  var tableHeight = document.getElementsByTagName('tr');
+  var rows = tableHeight.length;
 })();
