50d13d5cd2afdd4a8d974584dbdece4f0e19509d
Molly Kent <kent.molly@gmail.com>
2015-04-08 09:54:03 -0700

    add pretty background image and colors

--- scripts/newLoc.js
+++ scripts/newLoc.js
@@ -6,17 +6,8 @@
 
 	newShopForm.addEventListener('submit', function(event) {
 		event.preventDefault();
-		if (!event.target.newName.value) {
-			return alert('Location name cannot be blank');
-		}
-		if (!event.target.minCustHr.value) {
-			return alert('Min. customers per hour cannot be blank');
-		}
-		if (!event.target.maxCustHr.value) {
-			return alert('Max. customers per hour cannot be blank');
-		}
-		if (!event.target.avgDonutPurch.value) {
-			return alert('Avg. # donuts per customer cannot be blank');
+		if ((!event.target.newName.value) || (!event.target.minCustHr.value) || (!event.target.maxCustHr.value) || (!event.target.avgDonutPurch.value)) {
+			return alert('All fields must be filled in!');
 		}
 		var newShop = new DonutShop(event.target.newName.value, {minCust: Number.parseInt(event.target.minCustHr.value), maxCust: Number.parseInt(event.target.maxCustHr.value), avgDonPurch: Number.parseInt(event.target.avgDonutPurch.value)});
 		event.target.newName.value = '';
